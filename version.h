b800360f46199686f311664836f8abe7d9f9bd12
Marian Denes <mardenes@gmail.com>
2013-05-14 17:32:09 +0200

    Version 1.5
    In the List Views:
     - full row(s) selecting
     - enabling changing order of columns by dragging headers
     - adding sorting arrow indicator(s) to the header of column(s)
    
    Tabs:
     - Tabs appears as buttons to better recognize the active tab
     - Hot track of the tabs - the tab under the mouse pointer is highlighted

--- version.h
+++ version.h
@@ -15,18 +15,18 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR = 1;
-	static const long MINOR = 4;
-	static const long BUILD = 2;
-	static const long REVISION = 323;
+	static const long MINOR = 5;
+	static const long BUILD = 0;
+	static const long REVISION = 325;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 1139;
-	#define RC_FILEVERSION 1,4,2,323
-	#define RC_FILEVERSION_STRING "1, 4, 2, 323\0"
-	static const char FULLVERSION_STRING[] = "1.4.2.323";
+	static const long BUILDS_COUNT = 1155;
+	#define RC_FILEVERSION 1,5,0,325
+	#define RC_FILEVERSION_STRING "1, 5, 0, 325\0"
+	static const char FULLVERSION_STRING[] = "1.5.0.325";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 11;
+	static const long BUILD_HISTORY = 12;
 	
 
 }
