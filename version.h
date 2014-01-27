df61cbc7a847da65ddd3d6318f517005aeec2889
Marian Denes <mardenes@gmail.com>
2014-01-27 12:01:10 +0100

    Version 1.6.4 or higher - meanwhile only changes of version number

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "25";
+	static const char DATE[] = "26";
 	static const char MONTH[] = "01";
 	static const char YEAR[] = "2014";
 	static const char UBUNTU_VERSION_STYLE[] =  "14.01";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 6;
-	static const long BUILD  = 3;
+	static const long BUILD  = 4;
 	static const long REVISION  = 337;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 1320;
-	#define RC_FILEVERSION 1,6,3,337
-	#define RC_FILEVERSION_STRING "1, 6, 3, 337\0"
-	static const char FULLVERSION_STRING [] = "1.6.3.337";
+	#define RC_FILEVERSION 1,6,4,337
+	#define RC_FILEVERSION_STRING "1, 6, 4, 337\0"
+	static const char FULLVERSION_STRING [] = "1.6.4.337";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 14;
