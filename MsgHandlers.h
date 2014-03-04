f92a9a5afa9704c49d8117a3b661c8ae7fa5303d
Marian Denes <mardenes@gmail.com>
2014-03-04 23:18:45 +0100

    Version 1.6.5 or higher - fixed TabCtrl_OnNotify() + refactoring
    Fixed non-appearance of little arrows & other issues in ListViews
    
    Message handlers functions moved to the new MsgHandlers.cpp file

---
+++ MsgHandlers.h
@@ -0,0 +1,25 @@
+#ifndef MSGHANDLERS_H_INCLUDED
+    #define MSGHANDLERS_H_INCLUDED
+
+    #include <windows.h>
+    #include <windowsx.h>
+    #include <commctrl.h>   // For WM_NOTIFY message crackers
+    #include <tchar.h>
+
+    int
+    TabCtrl_OnLvnColumClick(HWND hwndTabCtrl, INT id, LPNMHDR pnm);
+
+    int
+    TabCtrl_OnCustomDraw(LPNMLVCUSTOMDRAW lplvcd);
+
+    void
+    TabCtrl_OnSize(HWND hwndTabCtrl, UINT state, int cx, int cy);
+
+    BOOL
+    TabCtrl_OnNotify(HWND hwndTabCtrl, INT id, LPNMHDR pnm);
+
+    void Histg_OnSize(HWND hwndHistogram, UINT /*state*/, int cx, int cy);
+
+    void Histg_OnPaint(HWND hwndHistogram);
+
+#endif // MSGHANDLERS_H_INCLUDED
--- WndProcs.cpp
+++ WndProcs.cpp
@@ -3,6 +3,7 @@
 
 #include "WndProcs.h"
 #include "CreateTabbedWindow.h"
+#include "MsgHandlers.h"
 #include "Constants.h"
 #include "Helpers.h"
 #include "Classic.h"
@@ -10,115 +11,6 @@
 #include <wingdi.h>
 
 
-//
-//  Process WM_NOTIFY message for window/dialog: TabCtrl
-//
-static BOOL TabCtrl_OnNotify(HWND hwndTabCtrl, INT id, LPNMHDR pnm)
-{
-
-    HWND               hwndFrom = pnm->hwndFrom;
-    LPNMLVCUSTOMDRAW   lplvcd   = nullptr;
-    Status           * pStatus  = (Status *) GetProp(hwndTabCtrl, cn::PointerToStatus);
-
-    switch (pnm->code)
-    {
-    case LVN_COLUMNCLICK:
-        LPNMLISTVIEW pNmLv;
-        int          column;
-        int          lastClickedColumn;    // It is increased by 1 to have oportunity to save it with + or -
-
-        pNmLv  = (LPNMLISTVIEW) pnm;
-        column = pNmLv->iSubItem;          // Numbered from 0, in spite of deleting the original column zero
-        ++column;                          // Now numbered from 1
-
-        /*
-         *  Let the sorting by the column 3 ("Percent") is the same as the sorting by
-         *  the column 2 ("Count") to don't confuse the user by no reaction after
-         *  clicking alternately to the headers of the column 2 and column 3 -
-         *  now both of them will behave as one unit (changing the sorting direction)
-         */
-        if (column == 3)
-            column = 2;
-
-        lastClickedColumn = (id == cn::ListViewAlphaId) ?
-                             pStatus->getLastClickedColumnAlpha() :
-                             pStatus->getLastClickedColumnDigit();
-
-        /*
-         *  Changing the direction of the ordering to unprefered if it was
-         *  the click on the column's header sorted in the prefered order
-         */
-        if (column == lastClickedColumn)
-            column *= -1;                   // + means preferred, - means unpreferred order
-
-        (void)
-        ListView_SortItems(hwndFrom, cmpFunction, (LPARAM) column);
-        setHeadersArrows(hwndFrom, column);
-
-       if (id == cn::ListViewAlphaId)
-            pStatus->setLastClickedColumnAlpha(column);
-        else
-            pStatus->setLastClickedColumnDigit(column);
-
-        return 0;       // The return value is ignored
-
-    case NM_CUSTOMDRAW:
-
-        lplvcd = (LPNMLVCUSTOMDRAW) pnm;
-
-        switch(lplvcd->nmcd.dwDrawStage)
-        {
-
-        case CDDS_PREPAINT:
-            return CDRF_NOTIFYITEMDRAW;
-
-        case CDDS_ITEMPREPAINT:
-            SelectFont(lplvcd->nmcd.hdc,
-                         CreateFont
-                         (
-                           -16,                           // nHeight,
-                            0,                            // nWidth,
-                            0,                            // nEscapement,
-                            0,                            // nOrientation,
-                            FW_BOLD,                      // fnWeight,
-                            0,                            // fdwItalic,
-                            0,                            // fdwUnderline,
-                            0,                            // fdwStrikeOut,
-                            DEFAULT_CHARSET,              // fdwCharSet,
-                            OUT_DEFAULT_PRECIS,           // fdwOutputPrecision
-                            CLIP_DEFAULT_PRECIS,          // fdwClipPrecision,
-                            DEFAULT_QUALITY,              // fdwQuality,
-                            FF_DONTCARE | DEFAULT_PITCH,  // fdwPitchAndFamily,
-                            TEXT("Courier New")           // lpszFace
-                         ));
-            return CDRF_NEWFONT;
-        default:
-            break;
-        }
-    default:
-        break;
-    }
-    return TRUE;    // In this case the return value is ignored
-}
-
-
-//
-//  Process WM_SIZE message for window/dialog: TabCtrl
-//
-void TabCtrl_OnSize(HWND hwnd, UINT state, int cx, int cy)
-{
-    RECT     rect, * pRect  = &rect;
-    Status * pStatus        = (Status *) GetProp(hwnd, cn::PointerToStatus);
-    WNDPROC  oldTabCtrlProc = (WNDPROC) (pStatus->getOldTabCtrlWndProc());
-
-    FORWARD_WM_SIZE(hwnd, state, cx, cy, oldTabCtrlProc);  // Particularly for displaying arrows for short tabs row
-
-    // Computing of the new rectangle for the child windows
-    SetRect(pRect, 0, 0, cx, cy);
-    (void)
-    TabCtrl_AdjustRect(hwnd, FALSE, pRect);
-    pStatus->moveAllChildWindows(pRect);
-}
 
 
 LRESULT CALLBACK
