49407cb5ab78617fa44b4f9a18614ca938882b6c
Marian Denes <mardenes@gmail.com>
2014-03-21 16:22:52 +0100

    Version 1.7.1
    More font sizes in the Settings tab
    Full support of the keyboard in the Settings tab

--- version.h
+++ version.h
@@ -2,32 +2,32 @@
 #define VERSION_H
 
 namespace AutoVersion{
-
+	
 	//Date Version Types
-	static const char DATE[] = "12";
+	static const char DATE[] = "21";
 	static const char MONTH[] = "03";
 	static const char YEAR[] = "2014";
 	static const char UBUNTU_VERSION_STYLE[] =  "14.03";
-
+	
 	//Software Status
 	static const char STATUS[] =  "Release";
 	static const char STATUS_SHORT[] =  "r";
-
+	
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 7;
 	static const long BUILD  = 1;
-	static const long REVISION  = 337;
-
+	static const long REVISION  = 341;
+	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 1346;
-	#define RC_FILEVERSION 1,7,0,337
-	#define RC_FILEVERSION_STRING "1, 7, 0, 337\0"
-	static const char FULLVERSION_STRING [] = "1.7.0.337";
-
+	static const long BUILDS_COUNT  = 1407;
+	#define RC_FILEVERSION 1,7,1,341
+	#define RC_FILEVERSION_STRING "1, 7, 1, 341\0"
+	static const char FULLVERSION_STRING [] = "1.7.1.341";
+	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 14;
-
+	static const long BUILD_HISTORY  = 15;
+	
 
 }
 #endif //VERSION_H
